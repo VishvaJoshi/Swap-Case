eb04c023fc8b1b7aaa2c1e49a59154cc021c4046
Vishva Joshi <166016227+VishvaJoshi@users.noreply.github.com>
2024-05-31 15:05:25 +0100

    Add files via upload

---
+++ Swap Case.py
@@ -0,0 +1,15 @@
+def swap_case(string):
+    return string.swapcase()
+
+# Example usage
+input_string = "Hello World!"
+output_string = swap_case(input_string)
+print(output_string)
+
+
+string = "VisHva JosHI"
+
+print(string.swapcase())
+
+x = input('ENter your name: ')
+print(x.swapcase())
